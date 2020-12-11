b46019c2a769c33288fcb615d3ad11f3e274e635
Caio Montenegro <caio.zeu@hotmail.com>
2020-12-11 19:17:37 -0300

    Front-End e conteudos dinamicos passados de PUG para Handle-Bars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const expressHbs = require('express-handlebars')
 
 const path = require('path')
 const app = express()
@@ -7,7 +8,13 @@ const app = express()
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-app.set('view engine', 'pug')
+app.engine('hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  }))
+app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -17,7 +24,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Not Found'})
+  res.status(404).render('404', { pageTitle: 'Not Found', formsCSS: true, productsCSS: true, activeAddProduct: true })
 })
 
 app.listen(3000)
